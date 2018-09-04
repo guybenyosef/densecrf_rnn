66a70fc85275ba2ca2e61be590ab8ccee7d784e8
Guy on CSAIL <gby@melville.csail.mit.edu>
2018-09-04 10:51:21 -0400

    notes and useful commandlines

--- gby_script.sh
+++ gby_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env
 
+# installation:
+# ----------------
 conda create -n tensorflow pip python=3.5
 source /afs/csail.mit.edu/u/g/gby/libs/anaconda3/bin/activate tensorflow
 pip install --ignore-installed tensorflow-gpu==1.4
@@ -11,3 +13,13 @@ export KERAS_BACKEND=tensorflow
 cd src/cpp
 make
 
+# run test:
+# ----------
+source /afs/csail.mit.edu/u/g/gby/libs/anaconda3/bin/activate tensorflow
+export KERAS_BACKEND=tensorflow
+python run_demo.py
+python run_model_on_img.py /storage/gby/semseg/voc12_weights image.jpg
+
+# run train:
+# ----------
+python train.py
