97e5c63b2ed3ecc8caad6ac5496d0aef5646e705
Guy on CSAIL <gby@melville.csail.mit.edu>
2018-10-04 14:05:30 -0400

    adding conv filter visualization

--- utils_gby.py
+++ utils_gby.py
@@ -1,12 +1,15 @@
-# my own keras implementation for fcn8
+# utils
 
 import pdb
+from keras import backend as K
 from keras.applications.vgg16 import preprocess_input, decode_predictions
+from scipy.misc import imsave
 #from keras.applications.resnet50 import preprocess_input, decode_predictions
 from PIL import Image
 import numpy as np
 import copy
 import os
+import time
 import sys
 sys.path.insert(1, './src')
 import util
@@ -226,3 +229,123 @@ def give_color_to_seg_img(seg, n_classes):
         seg_img[:, :, 2] += (segc * (colors[c][2]))
 
     return (seg_img)
+
+# -----------------------
+# Visualization
+# -----------------------
+# from: https://blog.keras.io/how-convolutional-neural-networks-see-the-world.html
+
+def deprocess_image(x):
+    # normalize tensor: center on 0., ensure std is 0.1
+    x -= x.mean()
+    x /= (x.std() + K.epsilon())
+    x *= 0.1
+
+    # clip to [0, 1]
+    x += 0.5
+    x = np.clip(x, 0, 1)
+
+    # convert to RGB array
+    x *= 255
+    if K.image_data_format() == 'channels_first':
+        x = x.transpose((1, 2, 0))
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def visualize_conv_filters(model, INPUT_SIZE, layer_name):
+
+    kept_filters = []
+    for filter_index in range(10):
+        # we only scan through the first 200 filters,
+        # but there are actually 512 of them
+        print('Processing filter %d' % filter_index)
+        start_time = time.time()
+
+        # dimensions of the generated pictures for each filter.
+        if(INPUT_SIZE == None):
+            img_width = 128
+            img_height = 128
+        else:
+            img_width = INPUT_SIZE
+            img_height = INPUT_SIZE
+
+        # this is the placeholder for the input images
+        input_img = model.input
+
+        # get the symbolic outputs of each "key" layer (we gave them unique names).
+        layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
+
+        # build a loss function that maximizes the activation
+        # of the nth filter of the layer considered
+        layer_output = layer_dict[layer_name].output
+        if K.image_data_format() == 'channels_first':
+            loss = K.mean(layer_output[:, filter_index, :, :])
+        else:
+            loss = K.mean(layer_output[:, :, :, filter_index])
+
+        # compute the gradient of the input picture wrt this loss
+        grads = K.gradients(loss, input_img)[0]
+
+        # normalization trick: we normalize the gradient
+        grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+
+        # this function returns the loss and grads given the input picture
+        iterate = K.function([input_img], [loss, grads])
+
+        # step size for gradient ascent
+        step = 1.
+
+        # we start from a gray image with some noise
+        if K.image_data_format() == 'channels_first':
+            input_img_data = np.random.random((1, 3, img_width, img_height))
+        else:
+            input_img_data = np.random.random((1, img_width, img_height, 3))
+        input_img_data = (input_img_data - 0.5) * 20 + 128
+
+        # run gradient ascent for 20 steps
+        for i in range(20):
+            loss_value, grads_value = iterate([input_img_data])
+            input_img_data += grads_value * step
+
+            print('Current loss value:', loss_value)
+            if loss_value <= 0.:
+                # some filters get stuck to 0, we can skip them
+                break
+
+        # decode the resulting input image
+        if loss_value > 0:
+            img = deprocess_image(input_img_data[0])
+            kept_filters.append((img, loss_value))
+        end_time = time.time()
+        print('Filter %d processed in %ds' % (filter_index, end_time - start_time))
+
+    # pdb.set_trace()
+    # imsave('%s_filter_%d.png' % (layer_name, filter_index), img)
+
+    # we will stich the best 64 filters on a 8 x 8 grid.
+    n = 2
+
+    # the filters that have the highest loss are assumed to be better-looking.
+    # we will only keep the top 64 filters.
+    kept_filters.sort(key=lambda x: x[1], reverse=True)
+    kept_filters = kept_filters[:n * n]
+
+    # build a black picture with enough space for
+    # our 8 x 8 filters of size 128 x 128, with a 5px margin in between
+    margin = 5
+    width = n * img_width + (n - 1) * margin
+    height = n * img_height + (n - 1) * margin
+    stitched_filters = np.zeros((width, height, 3))
+
+    # fill the picture with our saved filters
+    for i in range(n):
+        for j in range(n):
+            img, loss = kept_filters[i * n + j]
+            width_margin = (img_width + margin) * i
+            height_margin = (img_height + margin) * j
+            stitched_filters[
+            width_margin: width_margin + img_width,
+            height_margin: height_margin + img_height, :] = img
+
+    # save the result to disk
+    imsave('%s_%s_stitched_filters_%dx%d.png' % (model.name,layer_name,n, n), stitched_filters)
