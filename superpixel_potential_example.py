f34854ee10d4eb774d9faa0c7bed2e4f973902f9
C <cfmata@mit.edu>
2018-11-07 00:30:33 -0500

    Fixed sp update

--- superpixel_potential_example.py
+++ superpixel_potential_example.py
@@ -1,6 +1,8 @@
 # sp tem
 import tensorflow as tf
 # may come in useful: tf.verify_tensor_all_finite
+import numpy as np
+
 s = tf.InteractiveSession()
 nb_classes = 3
 rows, cols = 4, 5
@@ -21,6 +23,19 @@ q_vals = tf.constant([[[0.9, 0.9, 0.9, 0.9, 0.9],
                        [0.4, 0.5, 0.9, 0.9, 0.9],
                        [0.7, 0.8, 0.8, 0.9, 0.9]]])
 
+q_vals_arr = [[[0.9, 0.9, 0.9, 0.9, 0.9],
+                       [0.9, 0.9, 0.9, 0.9, 0.9],
+                       [0.01, 0.01, 0.01, 0.01, 0.01],
+                       [0.01, 0.01, 0.01, 0.01, 0.01]],
+                      [[0.01, 0.01, 0.01, 0.01, 0.01],
+                       [0.01, 0.01, 0.01, 0.01, 0.01],
+                       [0.9, 0.9, 0.8, 0.7, 0.6],
+                       [0.9, 0.9, 0.81, 0.5, 0.4]],
+                      [[0.01, 0.01, 0.01, 0.01, 0.01],
+                       [0.01, 0.01, 0.01, 0.01, 0.01],
+                       [0.4, 0.5, 0.9, 0.9, 0.9],
+                       [0.7, 0.8, 0.8, 0.9, 0.9]]]
+
 sp_map = tf.constant([[1,1,1,2,2],
                       [1,1,1,2,2],
                       [3,3,4,4,5],
@@ -37,8 +52,6 @@ values = [tf.cast(tf.equal(sp_map,i), tf.float32) for i in range(1,length+1)]
 split_sp_map = tf.stack(values)
 bool_sp_map = tf.stack([tf.equal(sp_map,i) for i in range(1,length+1)])
 
-#q_vals = tf.random_uniform(shape=[nb_classes,4,5])
-
 # This will put True where the max prob label, False otherwise
 cond_max_label = tf.equal(q_vals, tf.reduce_max(q_vals,axis=0))
 max_label = tf.cast(cond_max_label, tf.float32)
@@ -48,26 +61,30 @@ not_max_label = -1*tf.subtract(max_label, 1)
 w_low = tf.constant(0.1)
 w_high = tf.constant(0.9)
 
-sp_out = tf.zeros(q_vals.shape)
+sp_out = tf.get_variable("sp_out", [nb_classes, rows, cols], dtype=tf.float32, initializer=tf.zeros_initializer)
+s.run(tf.global_variables_initializer())
 max_q_val = tf.multiply(max_label, q_vals)
 not_max_q_val = tf.multiply(not_max_label, q_vals)
 
-for l in range(length):
+for l in range(nb_classes):
     for i in range(rows):
         for j in range(cols):
             # clique index is value of (i,j) in sp_map
-            clique_index = sp_map[i][j]            
-            product_matrix = tf.Variable(tf.multiply(split_sp_map[clique_index-1], q_vals[l]))
-            init_op = tf.global_variables_initializer()
-            s.run(init_op)
-            print(s.run(product_matrix))
-            tf.assign(product_matrix[i][j], 0)
+            clique_index = sp_map[i][j]
+            product_matrix = tf.multiply(split_sp_map[clique_index-1], q_vals[l])
+            val = s.run(product_matrix[i][j])
             flattened = tf.reshape(product_matrix, [-1])
-            print(s.run(flattened))
-            # Want the product of all nonzero elements in 
-            product = tf.nonzero_product(flattened)
-            sp_out[l][i][j] += w_low * product + w_high*(1-product)
+            # Want the product of all nonzero elements in
+            zero = tf.constant(0, dtype=tf.float32)
+            where_nonzero = tf.not_equal(flattened, zero)
+            reduced = tf.boolean_mask(flattened, where_nonzero)
+            product = tf.reduce_prod(reduced)
+            # Must divide product by value at q_i = l
+            product = tf.divide(product, val)
+            op = sp_out[l,i,j].assign(sp_out[l,i,j]+product)
+            s.run(op)
 
+print(s.run(sp_out))
 
 '''
 for sp_indx in range(1,6):
