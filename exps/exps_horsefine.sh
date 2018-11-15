26c0d9e27cde21ddcf56c90ac913909afa33ddea
Guy on CSAIL <gby@melville.csail.mit.edu>
2018-11-15 09:36:41 -0500

    organizing exps to batches

--- exps/exps_horsefine.sh
+++ exps/exps_horsefine.sh
@@ -12,8 +12,8 @@ python train_gby.py -m fcn_RESNET50_8s_crfrnnSPIOAT -is 224 -ds horsefine -e $nu
 # /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSP_1ep
 
 
-python train_gby.py -m fcn_RESNET50_8s_crfrnn -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnn_1ep
-python train_gby.py -m fcn_RESNET50_8s_crfrnnSP -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSP_1ep
-python train_gby.py -m fcn_RESNET50_8s_crfrnnSPIO -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPIO_1ep
-python train_gby.py -m fcn_RESNET50_8s_crfrnnSPAT -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPAT_1ep
-python train_gby.py -m fcn_RESNET50_8s_crfrnnSPIOAT -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPIOAT_1ep
+#python train_gby.py -m fcn_RESNET50_8s_crfrnn -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnn_1ep
+#python train_gby.py -m fcn_RESNET50_8s_crfrnnSP -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSP_1ep
+#python train_gby.py -m fcn_RESNET50_8s_crfrnnSPIO -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPIO_1ep
+#python train_gby.py -m fcn_RESNET50_8s_crfrnnSPAT -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPAT_1ep
+#python train_gby.py -m fcn_RESNET50_8s_crfrnnSPIOAT -is 224 -ds horsefine -e $num_ep -bs 1 -w /storage/gby/semseg/horsefine_weights_fcn_RESNET50_8s_crfrnnSPIOAT_1ep
